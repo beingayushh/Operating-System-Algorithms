0d235a61f644e898b672e9cd5836a01925d42d3c
Ayush Gupta <77526719+ayush20501@users.noreply.github.com>
2021-12-11 15:28:32 +0530

    Update Lru Page Replacement.c

--- Lru Page Replacement.c
+++ Lru Page Replacement.c
@@ -1,93 +1,66 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
 
-void lru(int string[20], int n, int size)
-{
-    int frames[n];
+int main(){
+    int pages,f,string[50],frames[50];
+    printf("Enter pages: ");
+    scanf("%d",&pages);
 
-    for (int i = 0; i < n; i++)
+    printf("Enter frames: ");
+    scanf("%d",&f);
+
+    for(int i=0;i<pages;i++){
+        scanf("%d",&string[i]);
+    }
+
+    for(int i=0;i<f;i++){
         frames[i] = -1;
+    }
 
-    int index = -1;
+    int index=-1;
+    int page_faults=0;
+    int page_hits=0;
 
-    int page_miss = 0;
-    int page_hits = 0;
-
-    for (int i = 0; i < size; i++)
-    {
-        int symbol = string[i];
-        int flag = 0;
-
-        int full = 0;
-
-        for (int j = 0; j < n; j++)
-        {
-            if (symbol == frames[j])
-            {
-                flag = 1;
+    for(int i=0;i<pages;i++){
+        int symbol=string[i];
+        int full=0;
+        int flag=0;
+        for(int j=0;j<f;j++){
+            if(symbol==frames[j]){
+                flag=1;
                 break;
             }
         }
-
-        if (flag == 1)
-        {
-            page_hits += 1;
+        if(flag==1){
+            page_hits++;
         }
-        else
-        {
-
-            if (full == 0)
-            {
-                index = (index + 1) % n;
-                frames[index] = symbol;
-                page_miss += 1;
+        else{
+            if(full==0){
+                index=(index+1)%pages;
+                frames[index]=symbol;
+                page_faults++;
             }
-
-            else
-            {
-
-                int pos = 999;
-                int index = -1;
-
-                for (int j = 0; j < n; j++)
-                {
-                    for (int k = 0; k < size; k++)
-                    {
-                        if (frames[j] == string[k])
-                        {
-                            if (pos > k)
-                            {
-                                pos = k;
-                                index = j;
+            else{
+                int pos=999;
+                int index=-1;
+                for(int j=0;j<f;j++){
+                    for(int k=0;k<pages;k++){
+                        if(frames[j]==string[k]){
+                            if(pos>k){
+                                pos=k;
+                                index=j;
                                 break;
                             }
                         }
                     }
                 }
-
                 frames[index] = symbol;
             }
         }
     }
-    page_miss = page_miss-1;
-    printf("\nPage hits: %d", page_hits);
-    printf("\nPage faults: %d", page_miss);
-    printf("\nPage fault ratio: %.2f",(float)page_miss/size);
-    printf("\nPage hit ratio: %.2f",(float)page_hits/size);
-}
 
-int main(void)
-{
-    int pages,frames;
-    printf("Enter Pages: ");
-    scanf("%d",&pages);
-
-    printf("Enter Frames: ");
-    scanf("%d",&frames);
-
-    int a[50];
-    printf("Enter reference string: ");
-    for(int i=0;i<pages;i++){
-        scanf("%d",&a[i]);
-    }
-    lru(a, frames, pages);
+    printf("Pages Faults: %d",page_faults);
+    printf("\nPage Hits: %d",page_hits);
+    printf("\nPage Fault Ratio: %.2f",(float)page_faults/pages);
+    printf("\nPage Hit Ratio: %.2f",(float)page_hits/pages);
 }
